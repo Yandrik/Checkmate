a0d6f4f5d75145aae2b9ed04c2f95110e599c15d
Yandrik <me@yandrik.dev>
2025-05-23 21:42:46 +0200

    feat(extension): add skeleton ui framework and update deps

--- extension/src/entrypoints/content/Content.svelte
+++ extension/src/entrypoints/content/Content.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Counter from '@/lib/components/Counter.svelte';
+  import Counter from "@/lib/components/Counter.svelte";
 </script>
 
 <!-- Neon Pink (working) -->
 <div class="bg-neon-pink">Content!</div>
 
 <!-- Neon Pink (broke) -->
-<Counter class="bg-neon-pink" />
+<!-- Counter class="bg-neon-pink" /-->
