1f3c6bab37940e84ab9cc9f12286b7fd04b11fbe
Marc Hummel <mhummel@wildwolfwuff.de>
2025-05-24 20:51:49 +0200

    feat: use agent in controller

--- backend/controller.py
+++ backend/controller.py
@@ -12,6 +12,8 @@ from models import (
 import asyncio
 from ai_agent import Agent
 
+agent = Agent()
+
 class FactcheckController(Controller):
   path = "/factcheck"
   
@@ -23,47 +25,27 @@ class FactcheckController(Controller):
   async def handle_fact_check(self, data: FactCheckDetailsRequest) -> FactCheckResult:
     HttpUrl(data.url) # type: ignore[arg-type]
     self.logger.info(f"Received input: {data.title} -> {data.url}")
-    await asyncio.sleep(7)
-    return FactCheckResult(
-      score=0.5,
-      check_result=f"echo: {data.content}",
-      verdict=Verdict.UNSURE,
-      sources=[
-        FactCheckSource(
-          name=f"Echo Source: {data.title}",
-          link=data.url),
-        FactCheckSource(
-          name="Dummy Source",
-          link="https://example.com/dummy_source")
-      ])
+    return await asyncio.to_thread(agent.factcheck_whole_page,data) 
+    
 
   @post("/text")
   async def handle_fact_check_text(self, data: str) -> FactCheckResult:
     self.logger.info(f"Received text input: {data}")
-    await asyncio.sleep(7)
-    return FactCheckResult(
-      score=0.5,
-      check_result=f"echo: {data}",
-      verdict=Verdict.UNSURE,
-      sources=[
-        FactCheckSource(
-          name="Echo Text Source",
-          link="https://example.com/echo_text_source")
-      ])
+    raise NotImplementedError("TODO: Implement text fact check")
+    # return await asyncio.to_thread(agent.factcheck_text, data)
 
   @post("/socialmedia")
   async def handle_fact_check_socialmedia(self, data: SocialMediaDetailsRequest) -> FactCheckResult:
     # HttpUrl(data.url) # type: ignore[arg-type]
     self.logger.info(f"Received input: {data.username} -> {data.displayName}")
-    return FactCheckResult(
-      score=0.5,
-      check_result=f"echo: {data.content}",
-      verdict=Verdict.UNSURE,sources=[])
+    return await asyncio.to_thread(agent.factcheck_social_media, data)
   
   @post("/media")
   async def handle_fact_check_media(self,data: MediaDetailsRequest) -> FactCheckResult:
-    raise NotImplementedError("TODO: Implement media fact check")
+    self.logger.info(f"Received input: {data.title} -> {data.url}")
+    return await asyncio.to_thread(agent.factcheck_media_details, data)
   
   @post("/media/comment")
   async def handle_fact_check_media_comment(self, data: MediaCommentDetailsRequest) -> FactCheckResult:
-    raise NotImplementedError("TODO: Implement media comment fact check")
+    self.logger.info(f"Received input: {data.author} -> {data.channelUrl}")
+    return await asyncio.to_thread(agent.factcheck_media_comment, data)
